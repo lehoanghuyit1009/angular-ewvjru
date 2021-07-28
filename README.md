b154ec836e6712a64bfc9ea951998db5eaec7102
lehoanghuyit1009 <lehoanghuyit1009@gmail.com>
2021-07-28 17:23:39 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ewvjru
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ewvjru)
---
+++ src/app/product-detail/product-detail.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-detail',
+  styleUrls: ['product-detail.component.css'],
+  templateUrl: 'product-detail.component.html'
+})
+export class ProductDetailCompoment {}
